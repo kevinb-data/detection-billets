8c61c2c1912c9ed6f25fa8fed0d5937dcb96b371
kevinb-data <134632954+kevinb-data@users.noreply.github.com>
2023-05-25 18:33:47 +0200

    Add files via upload

--- example.py
+++ example.py
@@ -3,5 +3,23 @@
 # %% auto 0
 __all__ = []
 
-# %% test_streamlit.ipynb 2
+# %% test_streamlit.ipynb 3
+from datetime import datetime
+
+import streamlit as st
+
+from streamlit_jupyter import StreamlitPatcher, tqdm
+
+# %% test_streamlit.ipynb 5
 st.title("Example")
+
+# %% test_streamlit.ipynb 6
+st.markdown(
+    """
+
+This is a test page demonstrating the use of `streamlit_jupyter`.
+
+If you're seeing this in jupyter, then it's working!
+
+"""
+)
