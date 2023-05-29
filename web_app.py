b3d468ea2f83352d942d3bb27cc63e9ad8c2b43c
kevinb-data <134632954+kevinb-data@users.noreply.github.com>
2023-05-29 15:48:04 +0200

    Add files via upload

--- web_app.py
+++ web_app.py
@@ -21,33 +21,37 @@ Veuillez svp faire votre sélection au sein des variables en side-bar pour obten
 """
 )
 
-# Setup file upload
-# global df
 uploaded_file = st.sidebar.file_uploader(label="Upload your Excel file", type = ['csv'])
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file, encoding='utf-8', sep=';')
-#     st.write(df)
-#     st.dataframe(df,3000,500)
+
 else:
-    st.write("⚠️ Attention, pas de fichier en input")
+    st.title("⚠️ Attention, pas de fichier en input")
 
 st.sidebar.header("Les paramètres du billet à l'étude")
 
 def user_input():
 #     diagonal = st.sidebar.slider('La diagonale du billet', 171.04, 173.01, 173.01)
-    diagonal = st.selectbox('la diagonale du billet', options=df['diagonal'].values.tolist(), index=0)
 #     height_left = st.sidebar.slider('La hauteur gauche du billet', 103.14, 104.88, 104.88)
 #     height_right = st.sidebar.slider('La hauteur droite du billet', 102.82, 104.95, 104.95)
 #     margin_low = st.sidebar.slider('La marge basse du billet', 2.98, 6.90, 6.90)
 #     margin_up = st.sidebar.slider('La marge haute du billet', 2.27, 3.91, 3.91)
-#     length = st.sidebar.slider('La diagonale du billet', 109.49, 114.40, 114.40)
+#     length = st.sidebar.slider('La longueur du billet', 109.49, 114.40, 114.40)
+
+    diagonal = st.sidebar.selectbox('la diagonale du billet', options=df['diagonal'].values, index=0)
+    height_left = st.sidebar.selectbox('la hauteur gauche du billet', options=df['height_left'].values, index=0)
+    height_right = st.sidebar.selectbox('la hauteur droite du billet', options=df['height_right'].values, index=0)
+    margin_low = st.sidebar.selectbox('la marge basse du billet', options=df['margin_low'].values, index=0)
+    margin_up = st.sidebar.selectbox('la marge haute du billet', options=df['margin_up'].values, index=0)
+    length = st.sidebar.selectbox('la longueur du billet', options=df['length'].values, index=0)
+    
     data={
         'diagonal':diagonal,
-#         'height_left': height_left,
-#         'height_right': height_right,
-#         'margin_low': margin_low,
-#         'margin_up': margin_up,
-#         'length': length
+        'height_left': height_left,
+        'height_right': height_right,
+        'margin_low': margin_low,
+        'margin_up': margin_up,
+        'length': length
     }
     
     billet_parametres=pd.DataFrame(data, index=[0])
@@ -63,4 +67,3 @@ st.write(df.iloc[:,0:6])
 st.subheader('La prédiction du billet est:')
 st.write(df.iloc[:,7:10])
 
-# st.write(df)
